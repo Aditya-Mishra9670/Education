bc3f0dee1cde2c71f4f24d79a59fcd9f108261ba
kriti-rana15 <kritirana874@gmail.com>
2025-01-14 20:31:14 +0530

    About page

---
+++ src/components/About/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste reiciendis libero incidunt quidem accusantium at asperiores totam nulla commodi reprehenderit omnis, inventore numquam veniam. Ducimus amet doloremque veritatis in non. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus ut culpa minima ipsum incidunt commodi, maxime mollitia, cum eos omnis obcaecati enim! Ex sequi suscipit error aliquid quibusdam quia excepturi.</h3>
+    </div>
+  )
+}
+
+export default About
