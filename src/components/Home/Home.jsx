a3a007244e7525a07a3100d01f26c71d4b3b5c83
kriti-rana15 <kritirana874@gmail.com>
2025-01-14 20:33:41 +0530

    Temp Home page

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../Header/Header'
+import Footer from '../Footer/Footer'
+function Home() {
+  return (
+    <div>
+        <h1>This is home page.</h1>
+    </div>
+  )
+}
+
+export default Home
