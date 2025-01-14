996345cf34d3c23aae9b76123bc59a406ec73746
kriti-rana15 <kritirana874@gmail.com>
2025-01-14 20:36:24 +0530

    Created Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import LoginPage from './components/Login/Login';
 import {useState} from 'react';
-function App(){
-  
-  return(
+import About from './components/About/About';
+import Terms from './components/TermsServices/Terms';
+import PrivacyPolicy from './components/PrivacyPolicy/Privacy';
+import SignUp from './components/Signup/Signup';
+import Home from './components/Home/Home';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Courses from './components/Courses/Courses';
+import Categories from './components/Categories/Categories';
+const App = () => {
+  return (
     <Router>
+      <Header />
       <Routes>
-        <Route path="/" element={<LoginPage/>} />
+        <Route path="" element={<Home />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<SignUp />} />
+      </Routes>
+      <Footer/>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/privacypolicy" element={<PrivacyPolicy />} />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 export default App;
